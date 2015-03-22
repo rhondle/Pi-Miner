1b6192c6199155f68087f8b06e033f9bbfce4fce
rhondle <martzy@gmail.com>
2015-03-21 23:24:04 -0700

    Initial Commit

---
+++ start-miner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+CONFIG_FILE=/usr/local/bin/.miner_opts.sh
+# wait a few seconds before launching the miner to ensure that the network has a chance to come up
+/bin/sleep 10
+if [ -f "${CONFIG_FILE}" ]; then
+/bin/sh ${CONFIG_FILE}
+else
+echo "Not configured! Please run 'mining-setup' to configure your miner!"
+fi
